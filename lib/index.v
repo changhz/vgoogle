706e99c3460601f2cd831826aa957416e86e0986
changhz <hz.chang@pm.me>
2023-02-15 19:04:27 -0500

    strip tags

--- lib/index.v
+++ lib/index.v
@@ -4,7 +4,12 @@ import regex {regex_opt}
 
 pub type String = string
 
-pub fn (s String) replace(pattern string, txt string) string {
+pub fn (s String) find(pattern string) []string {
   mut re := regex_opt(pattern) or {panic('well')}
-  return re.replace(s, txt)
+  return re.find_all_str(s)
+}
+
+pub fn (s String) replace(pattern string, txt string) String {
+  mut re := regex_opt(pattern) or {panic('well')}
+  return String(re.replace(s, txt))
 }
--- vgoogle.v
+++ vgoogle.v
@@ -14,6 +14,7 @@ fn main() {
   fprs.skip_executable()
 
   query := fprs.string('query', `q`, '', 'Make a query')
+  page := fprs.int('page', `p`, 1, 'Page Number')
 
   additional_args := fprs.finalize() or {
     eprintln(err)
@@ -31,16 +32,18 @@ fn main() {
 
   save_as := 'dist/output.html'
 
-  mut page := 0
-
   if query != '' {
-    url := '"https://www.google.com/search?q=$query&start=$page.str()"'
+    url := '"https://www.google.com/search?q=$query&start=${((page-1) * 10).str()}"'
     cmd := 'curl ${headers.join(" ")} -A $user_agent $url -o $save_as'
     println(cmd)
+
     res := os.execute(cmd)
     println(res.output)
 
     txt := treat_txt(save_as)
+
+    list_results(txt)
+
     os.write_file(save_as, txt) or {panic('ok')}
     return
   }
@@ -49,10 +52,32 @@ fn main() {
   println(fprs.usage())
 }
 
+fn list_results(txt string) {
+  link_pattern := r'<a(.*)href="(.*)"(.*)>(.*)</a>'
+  header_pattern := r'<h[1-6](.*)>(.*)</h[1-6]>'
+
+  links := String(txt).find(link_pattern)
+  for link in links {
+    mut s := String(link).replace(link_pattern, '\\3\n\\1')
+    s = s.replace(header_pattern, r'\1')
+    println(s+'\n')
+  }
+}
+
 fn treat_txt(save_as string) string {
   mut txt := String(os.read_file(save_as) or {panic('oh')})
-  txt = txt.replace(r'<style>(.*)</style>', '')
-  txt = txt.replace(r'<script(.*)</script>', '')
+
+  for tag in ['head', 'style', 'script', 'svg', 'cite'] {
+    txt = txt.replace(r'<'+tag+r'(.*)>(.*)</'+tag+r'>', '')
+  }
+
+  for tag in ['span', 'div', 'b', 'br', 'hr'] {
+    txt = txt.replace(r'<'+tag+r'(.*)>', '')
+    txt = txt.replace(r'</'+tag+r'>', '')
+  }
+
+  txt = txt.replace(r'<img(.*)src="(.*)"(.*)>', r'img:\1')
   txt = txt.replace(r'class="(.*)"', '')
+
   return txt
 }
