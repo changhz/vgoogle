639dcadb85c8c2a7706a0bd9a035b2f5b5c27e31
Nasko <66528033+Nasko-5@users.noreply.github.com>
2025-09-23 01:23:33 +0300

    fixes
    fixed path issues and replaced curl command with net.http stuff

--- vgoogle.v
+++ vgoogle.v
@@ -3,10 +3,22 @@ module main
 import os
 import flag
 import lib {String}
+import net.http
 
 const abort_key = ':q'
 
+const user_agent = '"\
+		Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) \
+		AppleWebKit/537.36 (KHTML, like Gecko) \
+		Chrome/110.0.0.0 Safari/537.36\
+	"'
+
+const link_pattern = r'<a(.*)href="(.*)"(.*)>(.*)</a>'
+const header_pattern = r'<h[1-6](.*)>(.*)</h[1-6]>'
+const img_pattern = r'<img(.*)src="(.*)"(.*)>'
+
 fn main() {
+
 	mut fprs := flag.new_flag_parser(os.args)
 	fprs.application('vgoogle')
 	fprs.version('0.1.0')
@@ -28,8 +40,8 @@ fn main() {
 	// TODO:
 	headers := []string{}
 
-	save_as := '$dist/output.html'
-
+	dist := os.temp_dir()
+	save_as := os.join_path(dist, 'output.html')
 	mut url := '\
 		https://www.google.com/search\
 		?q=${query}\
@@ -42,12 +54,19 @@ fn main() {
 	if tab != '' { url += '&tbm=$tab' }
 
 	if query != '' {
-		cmd := 'curl ${headers.join(" ")} -A $user_agent "$url" -o $save_as'
+		mut req := http.new_request(
+			http.Method.get,
+			url,
+			''
+		)
 
-		os.execute(cmd)
+		req.add_custom_header('User-Agent', user_agent) or { panic('Error while adding user agent! ${err}')}
+
+        res := req.do() or { panic("Failed to query! ${err}") }
+        os.write_file(save_as, res.body) or { panic("Failed to write response! ${err}") }
 
 		mut txt := txt_filter(save_as)
-		os.execute('rm $save_as')
+		os.rm(save_as) or { eprintln('Failed to remove temp file: $err') }
 
 		mut replace_img := ''
 		if image {replace_img = r'![ img ](\1)'}
@@ -62,17 +81,6 @@ fn main() {
 	println(fprs.usage())
 }
 
-const (
-	user_agent = '"\
-		Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) \
-		AppleWebKit/537.36 (KHTML, like Gecko) \
-		Chrome/110.0.0.0 Safari/537.36\
-	"'
-	dist = '/tmp'
-	link_pattern = r'<a(.*)href="(.*)"(.*)>(.*)</a>'
-	header_pattern = r'<h[1-6](.*)>(.*)</h[1-6]>'
-	img_pattern = r'<img(.*)src="(.*)"(.*)>'
-)
 
 fn list_results(txt string) string {
 	links := String(txt).find(link_pattern)
@@ -86,7 +94,8 @@ fn list_results(txt string) string {
 }
 
 fn txt_filter(save_as string) string {
-	mut txt := String(os.read_file(save_as) or {panic('oh')})
+	println(save_as)
+	mut txt := String(os.read_file(save_as) or {panic(err)})
 
 	mut strip_ls := []string{}
 
